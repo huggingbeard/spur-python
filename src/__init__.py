8a3049ddc0ec089fc1f6fc87059878ec7f1124be
DGoettlich <daniel.gottlich@mansfield.ox.ac.uk>
2026-04-16 13:26:23 +0200

    [chores] export spurtest funs

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
+# TODO: we might eventually want to just export
+# the user facing main functions and keep the utils internal
+
 from .spur import (
     haversine_distance,
     get_distance_matrix,
@@ -18,6 +21,26 @@ from .spurhalflife import (
     spurhalflife,
 )
 
+from .spurtest import (
+    SpurTestResult,
+    get_distmat_normalized,
+    get_R,
+    get_sigma_dm,
+    lvech,
+    getcbar,
+    _cholesky_upper,
+    getpow_qf,
+    get_ha_parm_I1,
+    get_ha_parm_I0,
+    spatial_i1_test,
+    spatial_i0_test,
+    get_sigma_residual,
+    get_ha_parm_I1_residual,
+    spatial_i1_test_residual,
+    spatial_i0_test_residual,
+    spurtest,
+)
+
 all = [
     "haversine_distance",
     "get_distance_matrix",
@@ -31,4 +54,23 @@ all = [
     "spurtransform",
     "get_transformation_stats",
     "HalfLifeResult",
+    "spatial_persistence",
+    "spurhalflife",
+    "SpurTestResult",
+    "get_distmat_normalized",
+    "get_R",
+    "get_sigma_dm",
+    "lvech",
+    "getcbar",
+    "_cholesky_upper",
+    "getpow_qf",
+    "get_ha_parm_I1",
+    "get_ha_parm_I0",
+    "spatial_i1_test",
+    "spatial_i0_test",
+    "get_sigma_residual",
+    "get_ha_parm_I1_residual",
+    "spatial_i1_test_residual",
+    "spatial_i0_test_residual",
+    "spurtest",
 ]
